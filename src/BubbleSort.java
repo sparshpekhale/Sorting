d0666927dfe27a017f74c80de3a999ee8607489d
Sparsh <sparsh8910@yahoo.com>
2021-02-03 10:15:37 +0530

    added new bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,8 +4,11 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int arr[] = {4,32,5,234,2,0,-2}
-		
+		int arr[] = {4,32,5,234,2,0,-2};
+		bubbleSort(arr);
+		for(int i=0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 	
 	static void bubbleSort(int[] arr) {  
@@ -14,7 +17,6 @@ public class BubbleSort {
          for(int i=0; i < n; i++){  
                  for(int j=1; j < (n-i); j++){  
                           if(arr[j-1] > arr[j]){  
-                                 //swap elements  
                                  temp = arr[j-1];  
                                  arr[j-1] = arr[j];  
                                  arr[j] = temp;  
