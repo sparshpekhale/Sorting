4ebf2bfa887d0acbb97bcfe22909cff473b12a29
Sparsh <sparsh8910@yahoo.com>
2021-02-03 09:52:01 +0530

    push 1

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {4,32,5,234,2,0,-2}
+		
+	}
+	
+	static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+  
+    }  
+}
